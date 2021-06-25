8f5f32285b32f14156f038b7d41b6c9f7802a3fb
Joaovsr25 <Joaovitorsr200@gmail.com>
2021-06-25 00:36:37 -0300

    First commit

---
+++ menus/gksbr.js
@@ -0,0 +1,67 @@
+const menu = (pushname, prefix, groupName ) => {
+    return `
+   ⚠️ MENU •GKร• ⚠️
+〓〓〓〓〓〓〓〓〓〓
+[❗] ⁿⁱᶜᵏƲsʋαʀισ: ${pushname}
+[❗] βσи∂ɛ: •GK'ร•
+[❗] ɢʀᴜᴘᴏ: ${groupName}
+
+ ★彡Iɴғᴏʀᴍᴀᴄ̧ᴏ̃ᴇs ᴅᴏ Bᴏᴛミ★
+[▪️] = ᴄᴏᴍᴀɴᴅᴏs ᴅᴏ ʙᴏᴛ
+[❓] = ɪɴғᴏʀᴍᴀᴄᴀᴏ sᴏʙʀᴇ ᴏ ᴄᴏᴍᴀɴᴅᴏ
+ https://instagram.com/oficialgks_
+ 
+─────────────────  
+       *_👥ɢʀᴜᴘᴏs👥_*
+─────────────────
+▪️| ${prefix}fechar
+▪️| ${prefix}abrir
+▪️| ${prefix} marcargks
+❓| ᴍᴇɴᴄɪᴏɴᴀ ᴛᴏᴅᴏs ᴍᴇᴍʙʀᴏs 
+▪️| ${prefix}aviso
+❓| ɴᴏᴛɪғɪᴄᴀ ᴛᴏᴅᴏs ᴍᴇᴍʙʀᴏs  
+▪️| ${prefix}apagar ᴹᵃʳᵠᵘᵉ ᵃ ᵐˢᵍ
+▪️| ${prefix}online
+▪️| ${prefix}ranklevel
+❓| ʀᴀɴᴋ ᴅᴏs ɴɪᴠᴇɪs
+
+─────────────────
+             *_🎲 Dɪᴠᴇʀsᴀ̃ᴏ 🎲_*
+─────────────────
+▪️| ${prefix}img (ᵀᵉˣᵗᵒ)
+❓| Busca uma Imagem  
+▪️| ${prefix}eu
+▪️| ${prefix}ppt (pedra, papel ou tesoura)
+❓| ᴊᴏɢᴀʀ ᴘᴇᴅʀᴀ, ᴘᴀᴘᴇʟ, ᴛᴇsᴏᴜʀᴀ  
+▪️| ${prefix}sn (Texto)
+❓| sɪᴍ ᴏᴜ ɴᴀᴏ ᴘᴀʀᴀ ᴜᴍᴀ ᴘᴇʀɢᴜɴᴛᴀ
+▪️| ${prefix}chance (Texto)
+❓| ᴅɪᴢ ᴀ ᴄʜᴀɴᴄᴇ ᴅᴇ ᴀʟɢᴏ  
+▪️| ${prefix}amongus
+❓| "ᴇxᴘᴜʟsᴀ" ᴀʟɢᴜᴇᴍ ᴅᴀ ɴᴀᴠᴇ
+▪️| ${prefix}ttt
+❓| ᴊᴏɢᴏ ᴅᴀ ᴠᴇʟʜᴀ
+▪️| ${prefix}omais 
+❓| sᴏʀᴛᴇɪᴀ ᴀʟɢᴜᴇᴍ✅  
+▪️| ${prefix}top5
+❓| ᴛᴏᴘ5 ᴍᴇᴍʙʀᴏs ᴅᴏ ɢʀᴜᴘᴏ
+▪️| ${prefix}casal
+❓| sʜɪᴘᴀ ᴏs ᴅᴏɪs ᴍᴀʀᴄᴀᴅᴏs
+▪️] ${prefix}ranklindos
+
+━━━━━━━━━━━━━━━━
+         ғᴇʀʀᴀᴍᴇɴᴛᴀs 
+━━━━━━━━━━━━━━━━
+[▪️] ${prefix}sticker
+[❓] ᴄʀɪᴀ ғɪɢᴜʀɪɴʜᴀs 
+[▪️] ${prefix}toimg
+[❓] ᴄᴏɴᴠᴇʀᴛᴇ ғɪɢᴜʀɪɴʜᴀ ᴇᴍ ɪᴍᴀɢᴇᴍ  
+[▪️] ${prefix}Img ᵀᵉˣᵗᵒ
+[❓] ʙᴜsᴄᴀ ɪᴍᴀɢᴇᴍ ᴅᴏ sᴇᴜ ᴛᴇxᴛᴏ  
+[▪️] ${prefix}tts pt ᵀᵉˣᵗᵒ
+[❓] ғᴀᴢ ᴏ ʙᴏᴛ ғᴀʟᴀʀ
+[▪️] ${prefix}togif
+[❓] ᴄᴏɴᴠᴇʀᴛᴇ ғɪɢᴜʀɪɴʜᴀ ᴇᴍ ɢɪғ     
+━━━━━━━━━━━━━━━━━━┈`
+}
+exports.menu = menu
